3ab839a767c862ee5b5783d7af47bb3791954715
Kingdoom <mohammedharb411@gmail.com>
2026-08-10 21:30:38 +0300

    Change ticket title to Arabic

--- src/commands/Ticket/ticket.js
+++ src/commands/Ticket/ticket.js
@@ -137,7 +137,7 @@ const panelMessage = interaction.options.getString("panel_message") || "Click th
 const dmOnClose = interaction.options.getBoolean("dm_on_close") !== false;
 
             const setupEmbed = createEmbed({ 
-                title: "Support Tickets", 
+                title: "فتح التكت من هنا", 
 description: panelMessage,
                 color: getColor('info')
             });
