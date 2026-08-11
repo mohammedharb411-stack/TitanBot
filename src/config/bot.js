a1ad9b581e57bd2dc067002ccdc50627c8526297
Kingdoom <mohammedharb411@gmail.com>
2026-08-11 17:52:17 +0300

    Update bot activity names in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "! 𝐀𝐁𝐔 𝐘𝐎𝐔𝐒𝐄𝐅", // required by Discord API, not shown in the client
-        state: "! 𝐀𝐁𝐔 𝐘𝐎𝐔𝐒𝐄𝐅",     // this is what people actually see
+        name: " ٌ ", // required by Discord API, not shown in the client
+        state: " ٌ ",     // this is what people actually see
         type: 0,               // Custom
       },
     ],
