f357a9be4762dca4846bb99075b082b0ffe3288a
Kingdoom <mohammedharb411@gmail.com>
2026-08-11 10:55:22 +0300

    Translate error messages to Arabic in claim.js

--- src/commands/Ticket/claim.js
+++ src/commands/Ticket/claim.js
@@ -20,11 +20,11 @@ export default {
 
         const permissionContext = await getTicketPermissionContext({ client, interaction });
         if (!permissionContext.ticketData) {
-            return await replyUserError(interaction, { type: ErrorTypes.VALIDATION, message: 'This command can only be used in a valid ticket channel.' });
+            return await replyUserError(interaction, { type: ErrorTypes.VALIDATION, message: 'لا يمكن استخدام هذا الأمر إلا في قناة تكت صالحة.' });
         }
 
         if (!permissionContext.canManageTicket) {
-            return await replyUserError(interaction, { type: ErrorTypes.PERMISSION, message: 'You need the `Manage Channels` permission or the configured `Ticket Staff Role` to claim tickets.' });
+            return await replyUserError(interaction, { type: ErrorTypes.PERMISSION, message: 'أنت بحاجة إلى إذن "إدارة القنوات" أو دور "موظف التكت" المُكوّن للمطالبة بالتكت التذاكر.' });
         }
 
         await claimTicket(interaction.channel, interaction.user);
