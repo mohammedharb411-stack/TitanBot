bfdbb0b8362059c53e23c961e822640fd6533706
Kingdoom <mohammedharb411@gmail.com>
2026-08-11 10:51:09 +0300

    Update command description to Arabic

--- src/commands/Ticket/claim.js
+++ src/commands/Ticket/claim.js
@@ -9,7 +9,7 @@ import { claimTicket } from '../../services/ticket.js';
 export default {
     data: new SlashCommandBuilder()
         .setName("claim")
-        .setDescription("Claims an open ticket, assigning it to you.")
+        .setDescription("يقوم هذا الإجراء باستلام تذكرة مفتوحة، ثم يسندها إليك..")
         .setDMPermission(false),
 
     async execute(interaction, guildConfig, client) {
