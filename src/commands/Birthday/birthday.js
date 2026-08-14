835820d9b0d613bf1e5a4c6f08ae36c904c37841
Kingdoom <mohammedharb411@gmail.com>
2026-08-14 11:05:04 +0300

    Translate birthday command descriptions to Arabic

--- src/commands/Birthday/birthday.js
+++ src/commands/Birthday/birthday.js
@@ -2,6 +2,7 @@ import { SlashCommandBuilder, MessageFlags, ChannelType } from 'discord.js';
 import { createEmbed, successEmbed } from '../../utils/embeds.js';
 import { replyUserError, ErrorTypes } from '../../utils/errorHandler.js';
 
+// استيراد وحدات الأوامر الفرعية
 import birthdaySet from './modules/birthday_set.js';
 import birthdayInfo from './modules/birthday_info.js';
 import birthdayList from './modules/birthday_list.js';
@@ -10,18 +11,22 @@ import nextBirthdays from './modules/next_birthdays.js';
 import birthdaySetchannel from './modules/birthday_setchannel.js';
 
 import { InteractionHelper } from '../../utils/interactionHelper.js';
+
 export default {
+    // تعريف بياتات الأمر الرئيسي /birthday
     data: new SlashCommandBuilder()
         .setName('birthday')
-        .setDescription('Birthday system commands')
+        .setDescription('أوامر نظام أعياد الميلاد')
+        
+        // الأمر الفرعي: set (ضبط عيد الميلاد)
         .addSubcommand(subcommand =>
             subcommand
                 .setName('set')
-                .setDescription('Set your birthday')
+                .setDescription('تحديد تاريخ عيد ميلادك')
                 .addIntegerOption(option =>
                     option
                         .setName('month')
-                        .setDescription('Birth month (1-12)')
+                        .setDescription('شهر الميلاد (1-12)')
                         .setRequired(true)
                         .setMinValue(1)
                         .setMaxValue(12)
@@ -29,54 +34,67 @@ export default {
                 .addIntegerOption(option =>
                     option
                         .setName('day')
-                        .setDescription('Birth day (1-31)')
+                        .setDescription('يوم الميلاد (1-31)')
                         .setRequired(true)
                         .setMinValue(1)
                         .setMaxValue(31)
                 )
         )
+        
+        // الأمر الفرعي: info (عرض معلومات)
         .addSubcommand(subcommand =>
             subcommand
                 .setName('info')
-                .setDescription('View birthday information')
+                .setDescription('عرض معلومات عيد الميلاد')
                 .addUserOption(option =>
                     option
                         .setName('user')
-                        .setDescription('User to check birthday for')
+                        .setDescription('المستخدم المراد الفحص عنه')
                         .setRequired(false)
                 )
         )
+        
+        // الأمر الفرعي: list (القائمة)
         .addSubcommand(subcommand =>
             subcommand
                 .setName('list')
-                .setDescription('List all birthdays in the server')
+                .setDescription('عرض جميع أعياد الميلاد في السيرفر')
         )
+        
+        // الأمر الفرعي: remove (حذف)
         .addSubcommand(subcommand =>
             subcommand
                 .setName('remove')
-                .setDescription('Remove your birthday')
+                .setDescription('حذف عيد ميلادك')
         )
+        
+        // الأمر الفرعي: next (القادمة)
         .addSubcommand(subcommand =>
             subcommand
                 .setName('next')
-                .setDescription('Show upcoming birthdays')
+                .setDescription('عرض أعياد الميلاد القادمة')
         )
+        
+        // الأمر الفرعي: setchannel (تحديد قناة الإشعارات)
         .addSubcommand(subcommand =>
             subcommand
                 .setName('setchannel')
-                .setDescription('Set or disable the channel for birthday announcements. (Manage Server required)')
+                .setDescription('تعيين أو إلغاء قناة إعلانات أعياد الميلاد. (يتطلب صلاحية إدارة السيرفر)')
                 .addChannelOption(option =>
                     option
                         .setName('channel')
-                        .setDescription('The text channel for announcements. Leave empty to disable.')
+                        .setDescription('القناة النصية للإعلانات. اتركه فارغًا للإلغاء.')
                         .addChannelTypes(ChannelType.GuildText)
                         .setRequired(false)
                 )
         ),
 
+    // دالة تنفيذ الأمر عند استدعائه
     async execute(interaction, config, client) {
+        // الحصول على اسم الأمر الفرعي الذي تم اختياره
         const subcommand = interaction.options.getSubcommand();
 
+        // توجيه الطلب إلى الموديل المناسب
         switch (subcommand) {
             case 'set':
                 return await birthdaySet.execute(interaction, config, client);
@@ -91,7 +109,7 @@ export default {
             case 'setchannel':
                 return await birthdaySetchannel.execute(interaction, config, client);
             default:
-                return await replyUserError(interaction, { type: ErrorTypes.UNKNOWN, message: 'Unknown subcommand' });
+                return await replyUserError(interaction, { type: ErrorTypes.UNKNOWN, message: 'أمر فرعي غير معروف' });
         }
     }
 };
